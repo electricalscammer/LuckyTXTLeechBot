95688c0568f72d5b74cb931ad390f014e7d1aae1
electricalscammer <155905329+electricalscammer@users.noreply.github.com>
2024-01-08 00:24:25 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27881923
-api_hash = "79abda5e46a51fc0dce1313f2548ce19"
-bot_token = "6263666911:AAF_Zm2baKNCJMGPn8aCBlVArvTa-A4kz1k"
+api_id = 24262622
+api_hash = "50831eb3329ed9c0557aa2bc6aa34376"
+bot_token = "5881186368:AAE9NQzCRpKz8HA0uWaLCe-MimjSGrkvcWw"
 
